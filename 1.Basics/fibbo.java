fc4afe1ffe7f23b95b543f90681329153bbda544
dhairya-07 <dhairya3208@gmail.com>
2022-06-22 18:21:27 +0530

    Initial commit

---
+++ 1.Basics/fibbo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fibbo {
+
+  public static void main(String[] args) {
+    System.out.println("Which fibbo no. you want??");
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    int a = 0;
+    int b = 1;
+    int count = 2;
+    while (count <= n) {
+      int temp = b;
+      b = a + b;
+      a = temp;
+      count++;
+    }
+    System.out.println(n + "th fibbonachi number is " + b);
+    input.close();
+  }
+}
